beab2bbf018870e982b8cde3fc00bb1cba69c26a
cicchinj <cicchinj@oregonstate.edu>
2026-07-22 13:06:50 -0400

    Intial setup

--- export-microservice.js
+++ export-microservice.js
@@ -1,1 +1,25 @@
-console.log("Export Printable Microservice started.");
+// Microservice that receives JSON and returns formatted HTML
+
+import express from "express";
+import { formatHtml } from "./format-html.js";
+
+const app = express(); // create application
+const PORT = 3000;  // port listening on
+app.use(express.json()); // parse jSON
+
+// GET
+app.get("/", (req, res) => {
+    res.send("Export Printable Microservice is working.");
+});
+
+// POST
+app.post("/export", (req, res) => {
+    const html = formatHtml();
+
+    res.type("html").send(html);
+});
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log(`Export Printable Microservice is running on port ${PORT}`);
+});
---
+++ format-html.js
@@ -0,0 +1,16 @@
+export function formatHtml() {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Printable Document</title>
+            </head>
+
+            <body>
+                <h1>Printable Document</h1>
+                <p>Request received.</p>
+            </body>
+        </html>
+    `;
+}
