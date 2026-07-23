d3274a321f983eaba8c3404b3e842bef4c427004
Jason Saar <jason.saar@gmail.com>
2026-07-23 00:08:43 -0700

    Use full deck list as test document

--- post_request.js
+++ post_request.js
@@ -8,17 +8,56 @@ const doc = {
     subtitle: "Modern - 60 cards",
     sections: [
         {
-            heading: "Creatures (8)",
+            heading: "Creatures (10)",
             items: [
-                "4x Goblin Guide",
+                "4x Dragon's Rage Channeler",
+                "2x Goblin Guide",
                 "4x Monastery Swiftspear"
             ]
         },
         {
-            heading: "Spells (8)",
+            heading: "Sorceries (16)",
             items: [
-                "4x Lightning Bolt",
-                "4x Lava Spike"
+                "4x Boltwave",
+                "4x Lava Spike",
+                "4x Light Up the Stage",
+                "4x Skewer the Critics"
+            ]
+        },
+        {
+            heading: "Instants (8)",
+            items: [
+                "4x Lava Dart",
+                "4x Lightning Bolt"
+            ]
+        },
+        {
+            heading: "Artifacts (8)",
+            items: [
+                "4x Mishra's Bauble",
+                "4x Cori-Steel Cutter"
+            ]
+        },
+        {
+            heading: "Lands (18)",
+            items: [
+                "3x Arid Mesa",
+                "4x Barbarian Ring",
+                "1x Elegant Parlor",
+                "1x Sacred Foundry",
+                "3x Scalding Tarn",
+                "4x Snow-Covered Mountain",
+                "2x Sunbaked Canyon"
+            ]
+        },
+        {
+            heading: "Sideboard (15)",
+            items: [
+                "3x Tormod's Crypt",
+                "3x Meltdown",
+                "3x Unholy Heat",
+                "4x Molten Rain",
+                "2x Wear // Tear"
             ]
         }
     ]
